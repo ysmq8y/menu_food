d943b61a3e08486087431ec5695297d8ca4027fc
ysmq8y <ysmq8y@gmail.com>
2023-12-08 01:27:30 +0300

    added grid view but some changes are in order

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -8,10 +8,10 @@ class Food {
   Food({required this.foodImage, required this.foodName});
 
   static List<Food> foodlist = [
-    Food(foodImage: "${path}biryani.jpg", foodName: "biryani"),
-    Food(foodImage: "${path}burger.png", foodName: "burger"),
-    Food(foodImage: "${path}pasta.jpg", foodName: "pasta"),
-    Food(foodImage: "${path}pizza.jpg", foodName: "pizza"),
-    Food(foodImage: "${path}suchi.jpg", foodName: "suchi"),
+    Food(foodImage: "${path}royal.jpg", foodName: "العسل الملكي"),
+    Food(foodImage: "${path}white.jpg", foodName: "العسل الأبيض القرغيزي"),
+    Food(foodImage: "${path}zohor.jpg", foodName: "عسل زهورات جبلية"),
+    Food(foodImage: "${path}akasia.jpg", foodName: "عسل الأكاسيا"),
+    Food(foodImage: "${path}barb.jpg", foodName: "عسل برباريس"),
   ];
 }
--- lib/view/my_home_page.dart
+++ lib/view/my_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/food.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -12,6 +14,54 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "malathamin",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          backgroundColor: Color.fromARGB(222, 0, 0, 0),
+        ),
+        body: GridView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+              color: Color.fromARGB(222, 0, 0, 0),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                      Food.foodlist[index].foodName,
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    )),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      child: Image.asset(
+                        Food.foodlist[index].foodImage,
+                        height: 200,
+                        width: 200,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+          itemCount: Food.foodlist.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+          ),
+        ));
   }
 }
